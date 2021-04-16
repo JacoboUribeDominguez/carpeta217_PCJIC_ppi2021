47b0ce63e7d96052b4fd0703594662b677f621e2
Jacobo <jacouribe2003@gmail.com>
2021-04-16 13:24:18 -0500

    editando alguna estructura

---
+++ frontend/src/components/ComponenteEjemplo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    <b>Esto es un ejemplo de un componente</b>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ComponenteEjemplo",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>TITULO EJEMPLO</h1>
+    <ComponenteEjemplo msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import ComponenteEjemplo from "@/components/ComponenteEjemplo.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    ComponenteEjemplo,
   },
 };
 </script>
