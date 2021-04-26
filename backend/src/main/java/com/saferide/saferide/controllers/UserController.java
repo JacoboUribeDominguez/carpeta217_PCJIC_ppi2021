6e65f9bb9868d5ad57533c32f2a2c674dd944fda
Jacobo <jacouribe2003@gmail.com>
2021-04-26 01:39:29 -0500

    agregando tareas principales de registro de usuarios

--- backend/src/main/java/com/saferide/saferide/controllers/UserController.java
+++ backend/src/main/java/com/saferide/saferide/controllers/UserController.java
@@ -1,25 +1,64 @@
 package com.saferide.saferide.controllers;
 
+//importamos diferentes anotaciones y funciones a usar
 import com.saferide.saferide.models.UserModel;
 import com.saferide.saferide.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
-@RestController
+@RestController //indica que será una apirest
 @CrossOrigin(origins = "http://localhost:8080")
+/*
+    CrossOrigin es nuestro mejor amigo, las restapi por temas de seguridad han recurrido
+    a prohibir peticiones desde paginas http, esto se debe a que era la forma de hacer
+    ataque ciberneticos más fácil. CrossOrigin(origins = "http://localhost:8080") nos
+    acepta las peticiones desde nuestro local host. Esto mientras estamos en desarrollo
+    en producción será eliminado en caso de usar un host https. Si usamos cpanel indicarémos
+    la dirección ip del cpanel, puesto que cpanel es http, para comprobar pueden ingresar en
+    jakouribe.com.co y verán que aparece como url insegura.
+
+    Nuestro proyecto lo configuramos en el http://localhost/8081 para evitar choques con el frontend
+    que se ejecuta en http://localhost/8080.
+
+    es decir, crossorigin permite hacer peticiones a esta api desde http://localhost/8080.
+ */
 public class UserController {
-    @Autowired
+
+    @Autowired //esta etiqueta es quien nos deja insertar otra clase; En este caso, la interface userService
     UserService userService;
 
-    @GetMapping("/users")
-    public List<UserModel> getUsers(){
-        return(userService.getUsers());
-    }
+    /*
 
-    /*@PostMapping()
-    public UserModel saveUser(@RequestBody UserModel user){
-        return(userService.saveUser(user));
-    }*/
+        ¿Cómo funciona las peticiones a la api rest?
+        principalmente con las anotaciones mapping indicarémos que tipo de peticion es y la ruta que se debe seguir
+        Anotaciones que usaremos: @GetMapping @PostMapping @PutMapping @DeleteMapping.
+
+        ¿Cómo enrutamos las anotaciones mapping?
+        Se pone entre parentesís la ruta, ejemplo:
+        @GetMapping("/users")
+
+        Exactamente en la linea inferior a la anotación tiene que ir la función. Ejemplo:
+
+        Por favor ignoren todo lo que hay después de la flecha "<--" en cada linea
+
+        @GetMapping("/users") <-- Esta es la anotación mapping enrutada
+        public List<UserModel> getUsers(){ <-- sintaxis general para declarar una función List<UserModel> es el tipo de dato a retornar
+            return(userService.getUsers()); <-- retorna los datos
+        }
+
+     */
+    /*
+        TAREA: Inicio de sesión.
+        Asignación: Juan David Velasquez Toro
+    */
+
+    // Copie el código aquí
+
+    /*
+        TAREA: Registro de usuario.
+        Asignación: Luis Alejandro Calvo Alvarez
+    */
+
+
 }
