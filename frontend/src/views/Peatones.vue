cc9a67a0ab4f81c7b7ca5a9b0d348b302cfe51dc
Jacobo <jacouribe2003@gmail.com>
2021-04-30 13:48:07 -0500

    estructurando apartado peatones

---
+++ frontend/src/views/Peatones.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <!--El código va abajo de este comentario-->
+  </div>
+</template>
+
+<script>
+
+//css de peatones
+import "../styles/Peatones.css"
+
+export default {
+  name: "Peatones"
+}
+</script>
+
+<style scoped>
+
+</style>
