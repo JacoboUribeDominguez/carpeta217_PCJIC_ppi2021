e36f169f404d9f443bbcb45a66b6e8fe85e7563c
Luis Alejandro Calvo Álvarez <Offztinger@gmail.com>
2021-05-02 22:08:01 -0500

    Finalizando trabajo frontend 02-05-2021

--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -1,16 +1,28 @@
 <template>
-  <div>
-    escribe su código acá
+  <div class="contenedorPadre">
+    <p class="title">Registrate</p>
+    <div class="contenedorForm">
+      <form class="formulario">
+        <p>Usuario:</p>
+        <input id="username" placeholder="Digite su usuario" autocomplete="off">
+        <p>Nombre:</p>
+        <input id="name" placeholder="Digite su nombre" autocomplete="off">
+        <p>Correo:</p>
+        <input type="email" id="email" placeholder="Digite su correo electronico" autocomplete="off">
+        <p>Contraseña:</p>
+        <input type="password" id="password" placeholder="Digite su contraseña" autocomplete="off">
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
 
-import "../styles/register.css"
+import "../styles/register.css";
 
 export default {
   name: "Register"
-}
+};
 </script>
 
 <style scoped>
