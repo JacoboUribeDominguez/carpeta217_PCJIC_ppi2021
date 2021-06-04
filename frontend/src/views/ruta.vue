9376a5a93fb9853e4f1a5375ef7f0b6f541bcfb1
Jacobo <jacouribe2003@gmail.com>
2021-06-04 17:30:10 -0500

    arreglando bug de direccionamiento entre publicaciones

--- frontend/src/views/ruta.vue
+++ frontend/src/views/ruta.vue
@@ -3,12 +3,12 @@
         <div class="d-flex justify-content-center align-items-center col-md-8 col-12" style="background:black;">
             <div class="d-flex justify-content-between px-3" style="color:white;height:100%;position:absolute;width:100%;z-index:2;">
                 <div class="d-flex align-items-center">
-                    <svg @click="$router.push(`/ruta/${id}`)" v-if="id !== 0" xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
+                    <svg @click="goTo(id-1)" v-if="id !== 0" xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
                         <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
                     </svg>
                 </div>
                 <div class="d-flex align-items-center">
-                    <svg @click="$router.push(`/ruta/${id+2}`)" v-if="id !== (rutas.length-1)" style="transform:rotate(180deg);" xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
+                    <svg @click="goTo(id+1)" v-if="id !== (rutas.length-1)" style="transform:rotate(180deg);" xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
                         <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
                     </svg>
                 </div>
@@ -57,6 +57,11 @@ export default {
         return{   
             id : this.$route.params.id - 1,
             cambiado : false
+        }
+    },
+    methods : {
+        goTo(id){
+            this.id = id
         }
     },
     computed : {
