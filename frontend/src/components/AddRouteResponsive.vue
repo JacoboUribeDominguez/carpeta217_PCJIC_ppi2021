5e47e9a5c28707f58ddb66f3665b315ddbaa223a
Jacobo <jacouribe2003@gmail.com>
2021-06-07 22:19:09 -0500

    agregando limpieza al cerrar

--- frontend/src/components/AddRouteResponsive.vue
+++ frontend/src/components/AddRouteResponsive.vue
@@ -3,7 +3,7 @@
         <div class="AddRouteContainer">
             <div class="headerAddRouteContainer d-flex justify-content-between align-items-center">
                 <h4 class="d-flex align-items-center ml-3 my-3" style="margin:0;color:white;">Publicar ruta</h4>
-                <svg @click="alternateShowAddRoute()" style="color:white" xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-x-circle-fill my-2 mr-3 btnClose" viewBox="0 0 16 16">
+                <svg @click="close" style="color:white" xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-x-circle-fill my-2 mr-3 btnClose" viewBox="0 0 16 16">
                     <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
                 </svg>
             </div>
@@ -90,15 +90,15 @@
             onClick(){
                 this.$refs.addImage.click()
             },
-            alternateShowAddRoute(){
-                this.$emit('alternateShowAddRoute')
-            },
             uploadImg(){
                 let ubication = this.firstUbication + ' - ' + this.lastUbication;
                 this.$store.dispatch('uploadImgAction', { 
                     id : this.$cookies.get('token'),
                     ubication
                 })
+                this.close
+            },
+            close(){
                 this.firstUbication = ""
                 this.lastUbication = ""
                 this.$store.dispatch('changeImgAction')
