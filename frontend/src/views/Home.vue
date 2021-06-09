3cef8930f2485c8ace691ea8ba50336423fa527a
Jacobo <jacouribe2003@gmail.com>
2021-06-08 22:31:34 -0500

    agregando atomización

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -24,107 +24,11 @@
               <h1 class="text-muted" style="font-size:20px;">Algo esta mal, intentenlo mas tarde</h1>
             </div>
             <div class="publicaciones" v-for="(ruta, index) in rutas" :key="index" v-else>
-              <div class="publicacionDestacada" v-if="index < 3">
-                <p class="autor d-flex align-items-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="25"
-                    height="25"
-                    fill="currentColor"
-                    class="bi bi-person-circle mr-2"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                    <path
-                      fill-rule="evenodd"
-                      d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
-                    />
-                  </svg>
-                  {{ruta.id_usuario}}
-                </p>
-                <p class="descripcion">{{ruta.ubicacion}}</p>
-                <div class="d-flex align-items-center justify-content-center" style="background:black;min-height:auto;max-height:500px;overflow:hidden;width:100%;">
-                  <div class="my-5" v-if="!showImgs">
-                    <div class="spinner-border text-success" role="status">
-                      <span class="sr-only">Loading...</span>
-                    </div>
-                  </div>
-                  <img
-                    @click="$router.push(`/ruta/${(index+1)}`)"
-                    class="imagenes"
-                    :src="ruta.multimedia"
-                    alt="800x800"
-                    v-else
-                  />
-                </div>
-                <p class="descripcionMeGusta">{{ruta.me_gusta}}</p>
-                <div class="ml-2 d-flex">
-                  <button @click="like(ruta, index)" class="meGusta">
-                    <font-awesome-icon
-                      v-if="!ruta.liked"
-                      class="up"
-                      icon="thumbs-up"
-                      style="font-size: 1.5rem"
-                    />
-                    <font-awesome-icon
-                      v-else
-                      class="down"
-                      icon="thumbs-up"
-                      style="font-size: 1.5rem"
-                    />
-                  </button>
-                </div>
-              </div>
-              <div class="publicacion" @click="$router.push(`/ruta/${(index+1)}`)" v-else>
-                <p class="autor d-flex align-items-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="25"
-                    height="25"
-                    fill="currentColor"
-                    class="bi bi-person-circle mr-2"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                    <path
-                      fill-rule="evenodd"
-                      d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
-                    />
-                  </svg>
-                  {{ruta.id_usuario}}
-                </p>
-                <p class="descripcion">{{ruta.ubicacion}}</p>
-                <div class="d-flex align-items-center justify-content-center" style="background:black;min-height:auto;max-height:500px;overflow:hidden;width:100%;">
-                  <div class="my-5" v-if="!showImgs">
-                    <div class="spinner-border text-success" role="status">
-                      <span class="sr-only">Loading...</span>
-                    </div>
-                  </div>
-                  <img
-                    class="imagenes"
-                    :src="ruta.multimedia"
-                    alt="800x800"
-                    v-else
-                  />
-                </div>
-                <p class="descripcionMeGusta">{{ruta.me_gusta}}</p>
-                <div class="ml-2 d-flex">
-                  <button @click="like(ruta, index)" class="meGusta">
-                    <font-awesome-icon
-                      v-if="!ruta.liked"
-                      class="up"
-                      icon="thumbs-up"
-                      style="font-size: 1.5rem"
-                    />
-                    <font-awesome-icon
-                      v-else
-                      class="down"
-                      icon="thumbs-up"
-                      style="font-size: 1.5rem"
-                    />
-                  </button>
-                </div>
-              </div>
+              <Rute 
+                :index="index" 
+                :ruta="ruta"
+                :showImgs="showImgs"
+              />
             </div>
           </div>
         </div>
@@ -138,9 +42,14 @@
 // @ is an alias to /src
 // Aquí esta el css
 import "../styles/home.css";
+
+//components
 import Navbar from "../components/Navbar.vue";
 import AddRoute from "../components/AddRoute.vue";
 import AddRouteResponsive from "../components/AddRouteResponsive.vue";
+import Rute from "../components/rutes/Rute.vue"
+
+//librerias
 import { storage } from '../utils/firebase'
 const refStorage = storage.ref()
 // import Publicacion from "../components/Publicacion"
@@ -153,17 +62,22 @@ export default {
     Navbar,
     AddRoute,
     AddRouteResponsive,
+    Rute
     // Publicacion
   },
   data() {
     return {
-      showAddRoute: false
+      showAddRoute: false,
     };
   },
   methods: {
+
+    //Alternar rutas *****************************************************************
     alternateShowAddRoute(){
       this.showAddRoute = !this.showAddRoute;
     },
+
+    //Dar like ***********************************************************************
     like(ruta, index){
 
       const oldMe_gusta = this.rutas[index].me_gusta
@@ -265,58 +179,6 @@ h1 {
 img {
   width: 100%;
   height: auto;
-}
-
-.publicacion {
-  background: #312e2e;
-  border-radius: 10px;
-  margin: 1rem 0px;
-}
-
-.publicacionDestacada {
-  background-color: #312e2e;
-  box-shadow: 0px 0px 25px 1px rgb(253, 194, 0);
-  margin: 4rem 0px;
-}
-
-.publicacionDestacada:hover, .publicacion:hover {
-  border:1px solid #575151;
-  cursor:pointer;
-  transform: scale(1.1);
-}
-
-.autor {
-  border-bottom:2px solid #535353;
-  text-align: start;
-  padding: 10px;
-  color: white;
-}
-
-.descripcion {
-  text-align: start;
-  color: white;
-  padding: 0px 10px 0px;
-}
-
-.meGusta {
-  background: dodgerblue;
-  border-color: dodgerblue;
-  color: white;
-  border-radius: 10px;
-  padding: 0.5rem 0.5rem;
-  width: 20%;
-  margin-bottom: 1rem;
-}
-
-.meGusta .down {
-  transform : rotate(180deg)
-}
-
-.descripcionMeGusta {
-  padding: 10px 10px 10px;
-  margin: 0px 0px;
-  text-align: start;
-  color: white;
 }
 
 .btnNavegacion1 {
