e75fef3f3a56b5a5a65ee2f4027f2b09ca3fb4ab
Jacobo <jacouribe2003@gmail.com>
2021-07-29 15:58:09 -0500

    haciendo cambios al aspecto visual de las rutas exactas

--- frontend/src/views/Ruta.vue
+++ frontend/src/views/Ruta.vue
@@ -18,16 +18,44 @@
                   <span class="sr-only">Loading...</span>
                 </div>
             </div>
-            <div v-else>
-                <img ref="imgRef" :src="rutas[id].multimedia"/>
+            <div class="containerImgRef" v-else>
+                <img ref="imgRef" :src="rutas[id].multimedia" :alt="rutas[id].id_ruta"/>
             </div>
         </div>
         <div class="col-md-4 col-12" style="background:#212121;padding:0;">
-            <div class="py-3 headerComments d-flex justify-content-between" style="color:#EEEEEE;">
+            <div class="pt-3 pb-2 d-flex justify-content-between" style="color:#EEEEEE;">
                 <h4 class="d-flex align-items-center ml-3" style="margin:0;">{{(rutas[id]) ? rutas[id].ubicacion : ''}}</h4>
                 <svg @click="$router.push('/')" style="color:white" xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-x-circle-fill my-2 mr-3 btn-remove-rute" viewBox="0 0 16 16">
                     <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
                 </svg>
+            </div>
+            <div class="headerComments px-3 d-flex justify-content-start align-items-center pb-3" style="color:white">
+                <button class="meGusta mr-2">
+                    <font-awesome-icon
+                        class="up"
+                        icon="thumbs-up"
+                        style="font-size: .8rem"
+                    />
+                </button>
+                <div>
+                    {{rutas[id].me_gusta}}
+                </div>
+            </div>
+            <div class="d-flex justify-content-start ml-3">
+                <div class="giveLike" style="color:white;">
+                    <font-awesome-icon
+                        v-if="!rutas[id].liked"
+                        class="up"
+                        icon="thumbs-up"
+                        style="font-size: 1rem"
+                    />
+                    <font-awesome-icon
+                        v-else
+                        class="down"
+                        icon="thumbs-up"
+                        style="font-size: 1rem"
+                    />
+                </div>
             </div>
             <div class="d-flex flex-column justify-content-center align-items-center p-4">
                 <div class="d-flex justify-content-center inputCommentContainer px-2">
@@ -67,15 +95,14 @@ export default {
             if(!this.cambiado){
                 if(this.$refs.imgRef){
                     const img = new Image();
-                    img.src = this.rutas[this.id].multimedia
+                    img.src = this.rutas[this.id].multimedia;
                     if(img.width > img.height){
-                        this.$refs.imgRef.classList="widthHigher"
+                        this.$refs.imgRef.classList="widthHigher";
                     } else if(img.width < img.height) {
-                        this.$refs.imgRef.classList="heightHigher"
+                        this.$refs.imgRef.classList="heightHigher";
                     } else {
-                        this.$refs.imgRef.classList="widthIqualHeight"
+                        this.$refs.imgRef.classList="widthIqualHeight";
                     }
-                    this.cambiado = true
                 }
             }
         }
@@ -105,7 +132,7 @@ export default {
 
     .widthHigher {
         height:auto;
-        max-width:100%;
+        max-width:85%;
     }
 
     .heightHigher {
@@ -115,10 +142,23 @@ export default {
 
     .widthIqualHeight {
         max-height:100vh;
-        max-width:100%;
+        width:100%;
     }
 
     /* COMMENTS */
+
+    .meGusta {
+        /* background:#444444; */
+        background: #24c567;
+        border:none;
+        border-radius:50%;
+        /* color:#24c567; */
+        color:#fff;
+        margin-bottom:0px;
+        padding:.2rem .5rem;
+        width:auto;
+    }
+
     .headerComments {
         border-bottom:1px solid #535353;
     }
@@ -147,6 +187,26 @@ export default {
 
     .btn-remove-rute:hover, .bi-arrow-left-circle-fill {
         cursor:pointer;
+    }
+
+    .containerImgRef{
+        align-items: center;
+        display:flex;
+        height:100%;
+        justify-content:center;
+        width:auto;
+    }
+
+    .giveLike {
+        color:white;
+        background: #535353;
+        border-bottom-left-radius: 50%;
+        border-bottom-right-radius: 50%;
+        padding: .5rem 1rem;
+    }
+
+    .down {
+        transform : rotate(180deg)
     }
 
 </style>
