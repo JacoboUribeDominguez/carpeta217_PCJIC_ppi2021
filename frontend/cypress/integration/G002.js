4cb4d3aa2c12cdd646754fdf43cafe00f627753e
Luis Alejandro Calvo Álvarez <Offztinger@gmail.com>
2021-12-03 21:02:46 -0500

    Subiendo pruebas unitarias

--- frontend/cypress/integration/G002.js
+++ frontend/cypress/integration/G002.js
@@ -5,4 +5,10 @@ describe("Login test case: ", function() {
     cy.get(".password").type("123");
     cy.get(".btnInicioSesion").click();
   });
+  it("Login failed: ", function() {
+    cy.visit("http://localhost:8080/Login");
+    cy.get(".user").type("MadHacker");
+    cy.get(".password").type("123");
+    cy.get(".btnInicioSesion").click();
+  });
 });
