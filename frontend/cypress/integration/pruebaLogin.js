e603e95347c0da3e78287e8eba9dd55ceba82b0b
Luis Alejandro Calvo Álvarez <Offztinger@gmail.com>
2021-12-03 20:21:28 -0500

    Subiendo cambios Cypress

---
+++ frontend/cypress/integration/pruebaLogin.js
@@ -0,0 +1,8 @@
+describe("Login test case: ", function() {
+  it("Login: ", function() {
+    cy.visit("http://localhost:8080/Login");
+    cy.get(".user").type("MadHacker");
+    cy.get(".password").type("123");
+    cy.get(".btnInicioSesion").click();
+  });
+});
